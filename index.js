66f9ae386491c7f67794a598edbe333d4520c780
Abhay <abhayachandra271@gmail.com>
2025-08-14 21:42:20 +0530

    task creating app

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    fs.readdir('./files', (err, files) => {
+        res.render('index', { files:files });
+    });
+});
+
+app.post('/bolimaga', (req, res) => {
+    fs.writeFile(`./files/${req.body.url.split(' ').join('')}.txt`, req.body.details, (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+    res.redirect('/');
+});
+
+app.listen(3000);
+
+
